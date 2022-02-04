887cb67494da3b728ec8cd5a4043f54fee4002a2
Elif Çifci <eliffbat@gmail.com>
2022-02-05 01:52:31 +0300

    Fixed react

--- REACT/homework-2/src/App.js
+++ REACT/homework-2/src/App.js
@@ -3,28 +3,50 @@ import './App.css';
 import Form from './Component/Form/index.js'
 import List from './Component/List/index.js'
 import Filter from './Component/Filter/index.js'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
+  const [isSelectAll, setIsSelectAll] = useState(false);
   const [list, setList] = useState([
     { text: "Taste JavaScript", done: true },
-    { text: "Code furiously", done: true },
-    { text: "Promote Mavo", done: false },
+    { text: "Code furiously", done: false },
+    { text: "Promote Mavo", done: true },
     { text: "Give talks", done: false },
-    { text: "Write tutorials", done: true },
-    { text: "Have a life!", done: false }
-  ]);
+    { text: "Have a life!", done: true },
+    { text: "Write tutorials", done: false }]);
+
+  useEffect(() => updateList(), [isSelectAll]);
+
+  const selectItem = (index) => {
+    let copyList = [...list];
+    copyList[index].done = !copyList[index].done;
+    setList(copyList);
+  }
+
+  const selectAllItems = () => {
+    setIsSelectAll((prevState) => !prevState);
+  }
+
+  const deleteItem = (index) => {
+    let copyList = [...list];
+    copyList.splice(index, 1);
+    setList(copyList);
+  }
+
+  const updateList = () => {
+    let copyList = [...list];
+    copyList.forEach(listItem => {
+      listItem.done = isSelectAll
+    });
+    setList(copyList);
+  }
 
   return (
     <div className={"todoapp"}>
-      <Form list={list} setList={setList} />
-
-      <div className={"main"}>
-      <List list={list} setList={setList} />
+      <Form list={list} setList={setList} selectAllItems={selectAllItems} isSelectAll={isSelectAll} />
+      <List list={list} selectItem={selectItem} deleteItem={deleteItem} />
       <Filter list={list} />
-      </div>
-
     </div>
   );
 }
--- REACT/homework-2/src/Component/Filter/index.js
+++ REACT/homework-2/src/Component/Filter/index.js
@@ -2,29 +2,61 @@ import { useState } from 'react';
 const buttonDOM = document.getElementsByClassName("filterButton")
 
 function Filter({ list }) {
-    const [isSelected, setIsSelected] = useState(false);
+    const [filteredList, setFilteredList] = useState("all");
+    
 
     const onClick = (event) => {
-        Object.values(buttonDOM).map((item)=> item.classList.remove("selected"))
+        Object.values(buttonDOM).map((item) => item.classList.remove("selected"))
         event.target.classList.toggle("selected");
+
+        if (event.target.id == "all") {
+            allButton();
         }
 
+        if (event.target.id == "active") {
+            activeButton();
+        }
+
+        if (event.target.id == "completed") {
+            completedButton();
+        }
+    }
+
+    const allButton = () => {
+        setFilteredList("all")
+    }
+
+    const activeButton = () => {
+        const copyList = [...list];
+        copyList.filter((item) => {
+            item.done = false;
+        })
+        setFilteredList(copyList)
+    }
+
+    const completedButton = () => {
+        console.log("completedButton");
+    }
+
+
+
+
     return <div className={"filters"}>
-        
-        <span className={"todo-count" }>
+
+        <span className={"todo-count"}>
             <strong>{list.length}</strong> items left
         </span>
         <ul>
             <li>
-                <button className={"filterButton"} onClick={onClick}>All</button>
+                <button id={"all"} className={"filterButton"} onClick={onClick}>All</button>
             </li>
 
             <li>
-                <button className={"filterButton"} onClick={onClick} >Active</button>
+                <button id={"active"} className={"filterButton"} onClick={onClick} >Active</button>
             </li>
 
             <li>
-                <button className={"filterButton"} onClick={onClick}>Completed</button>
+                <button id={"completed"} className={"filterButton"} onClick={onClick}>Completed</button>
             </li>
         </ul>
     </div>;
