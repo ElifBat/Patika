b7ef96e90d5ddadd315b28623aff522a1df2bb2a
Elif Çifci <eliffbat@gmail.com>
2022-02-09 04:08:48 +0300

    Used exact prop and tried url parameters.

---
+++ REACT/exercise-4/src/Component/User.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react/cjs/react.development";
+import { useParams, Link } from "react-router-dom";
+
+function User() {
+    const [user, setUsers] = useState({});
+    const [isLoading, setIsLoading] = useState(true)
+    let { id } = useParams();
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then((res) => { res.json() })
+            .then((data) => { setUsers(data) })
+            .finally(() => { setIsLoading(false) })
+    }, [id])
+
+    return (
+        <div>
+            <h1>User Details</h1>
+            {isLoading && <p>Loading</p>}
+            <pre>id: {id}</pre>
+            {!isLoading && <code>{JSON.stringify(user)}</code>}
+            <br /><br />
+            {<Link to={`/user/${parseInt(id) + 1}`}>
+                Next User ({parseInt(id) + 1})
+            </Link>}
+        </div>
+    )
+};
+
+export default User;
